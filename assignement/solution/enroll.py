cf5a7a8feeab5d3dc22c9a03fdb67243c7191a45
MamanAhmed <maman.ahmed95@gmail.com>
2025-02-28 16:23:08 +0100

    assignement finished

--- assignement/solution/enroll.py
+++ assignement/solution/enroll.py
@@ -1,2 +1,20 @@
+from course import Course
+from student import Student
+from datetime import datetime
+
+
 class Enroll:
-    pass
+    def __init__(self, student, course):
+        if not isinstance(student, Student):
+            raise Error("Invalid Student...")
+
+        if not isinstance(course, Course):
+            raise Error("Invalid Course...")
+
+        self.student = student
+        self.course = course
+        self.grade = None
+        self.date = datetime.now()
+
+    def set_grade(self, grade):
+        self.grade = grade
